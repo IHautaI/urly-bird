5b06ac624e5e8fee2fd273d31e0b4c54553b063a
Jeremy Gresham <greshjs@gmail.com>
2015-06-18 14:31:03 -0400

    started views

--- urly/urlyapp/tests.py
+++ urly/urlyapp/tests.py
@@ -1,3 +1,11 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
-# Create your tests here.
+from urlyapp.views import HomePage
+
+
+class HomePageTest(TestCase):
+
+    def test_root_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, HomePage)
